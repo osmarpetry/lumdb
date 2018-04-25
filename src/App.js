836563a9de93437d11bfd8cfe4f6dca66b00c70a
osmarpetry <osmarpetry@gmail.com>
2018-04-25 09:49:53 -0300

    fetch api to themoviedb

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import './App.css';
 
 import Movie from './components/Movie';
 
-const movies = [
-  {
-    id: 1,
-    title: 'Mr. Nobody'
-  },
-  {
-    id: 2,
-    title: 'Spider Man',
-    desc: 'Marvel Super-Hero Movie'
-  },
-  {
-    id: 3,
-    title: 'The Fifth Element'
-  }
-];
-
 class App extends Component {
+  state = {
+    movies: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=ddc64ae5e8e8de2f777406819ea8ee0f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+      );
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,20 +4,14 @@ import PropTypes from 'prop-types';
 export default class Movie extends Component {
   static propTypes = {
     movie: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-    }),
-    desc: PropTypes.string
-  };
-
-  static defaultProps = {
-    desc: 'Description not available'
+      title: PropTypes.string.isRequired
+    })
   };
 
   render() {
     return (
       <div>
         <h3>{this.props.movie.title}</h3>
-        <p>{this.props.desc}</p>
       </div>
     );
   }
