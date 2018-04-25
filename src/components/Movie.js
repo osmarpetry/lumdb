9291b3ed9c42c62f539aba402697f790baa96b15
osmarpetry <osmarpetry@gmail.com>
2018-04-25 10:51:49 -0300

    to stateless component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Movie extends Component {
-  static propTypes = {
-    movie: PropTypes.shape({
-      title: PropTypes.string.isRequired
-    })
-  };
+const Movie = ({ movie }) => (
+  <div>
+    <h3>{movie.title}</h3>
+  </div>
+);
 
-  render() {
-    return (
-      <div>
-        <h3>{this.props.movie.title}</h3>
-      </div>
-    );
-  }
-}
+export default Movie;
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired
+  }).isRequired
+};
