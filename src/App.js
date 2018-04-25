08239c85d6c107640cbdd9577557a828894fe167
osmarpetry <osmarpetry@gmail.com>
2018-04-25 06:22:20 -0300

    full control of input

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    input: 'Hello'
+  };
+
+  updateInput = event => {
+    console.log(event.target.value);
+    this.setState({
+      input: event.target.value
+    });
+  };
+
   submit = () => {
-    console.log(this.text);
+    console.log(this.message.value);
   };
 
   render() {
@@ -14,10 +25,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro" ref={input => (this.text = input)}>
+        <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Welcome message="IT'S A SMARTER PROP MENSSAGE!" />
+        <input
+          type="text"
+          onChange={this.updateInput}
+          value={this.state.input}
+        />
+        <input type="text" ref={input => (this.message = input)} />
         <button onClick={this.submit}> Show Value </button>
       </div>
     );
