f17d1643014f642ded058403a10ba4c6ac90d4d0
osmarpetry <osmarpetry@gmail.com>
2018-07-13 15:53:32 -0300

    integration with redux-dev-tools

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import logo from './logo.svg';
 import './App.css';
@@ -11,7 +12,7 @@ import rootReducer from './rootReducer';
 import MoviesList from './components/MoviesList';
 import MovieDetails from './components/MovieDetails';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, {}, composeWithDevTools());
 
 const App = () => (
   <Provider store={store}>
