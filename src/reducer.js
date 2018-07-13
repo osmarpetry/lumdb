4171f17c40b105d47e2e7edded4efda56aadfd8e
osmarpetry <osmarpetry@gmail.com>
2018-07-13 17:07:02 -0300

    const variables to have action variables to not use String

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,12 @@
+import { TOGGLE_MESSAGE } from './actions';
+
 const initialState = {
   messageVisibility: false
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'TOGGLE_MESSAGE':
+    case TOGGLE_MESSAGE:
       return { ...state, messageVisibility: !state.messageVisibility };
     default:
       return state;
