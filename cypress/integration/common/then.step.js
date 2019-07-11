d6327f79cf8b8c2fcbb1cb15d57305bce0d28033
osmarpetry <osmarpetry@gmail.com>
2019-07-10 23:54:29 -0300

    move tests to cucumber

--- cypress/integration/common/then.step.js
+++ cypress/integration/common/then.step.js
@@ -3,3 +3,11 @@ import { Then } from 'cypress-cucumber-preprocessor/steps'
 Then('I should see {string} in the url', url => {
     cy.url().should('include', url)
 })
+
+Then('I should\'t see {string} in the url', url => {
+    cy.url().should('not.include', url)
+})
+
+Then('I should see {string}', content => {
+    cy.contains(content)
+})
--- cypress/integration/common/when.step.js
+++ cypress/integration/common/when.step.js
@@ -1,5 +1,9 @@
 import { When } from 'cypress-cucumber-preprocessor/steps'
 
-When('I click on {string}', button => {
-    cy.get(`button[name=${button}]`).click()
+When('I click on href {string}', href => {
+    cy.get(`[href="/${href}"]`).click()
+})
+
+When('I click on first element a', () => {
+    cy.get('a').first().click()
 })
