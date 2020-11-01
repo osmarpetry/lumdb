bba4ba303cd7bca7a78c084241573070e27d42c7
osmarpetry <osmarpetry@gmail.com>
2020-11-01 18:58:57 -0300

    lazy router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import logo from './logo.svg'
 import './App.css'
 
-import MoviesList from 'container/MovieList'
-import MovieDetails from 'container/MovieDetails'
+const MoviesList = lazy(() => import('container/MovieList'))
+const MovieDetails = lazy(() => import('container/MovieDetails'))
 
 const App = () => (
     <Router>
         <div className='App'>
             <header className='App-header'>
                 <Link to='/'>
-                    <img src={ logo } className='App-logo' alt='logo' />
+                    <img src={logo} className='App-logo' alt='logo' />
                 </Link>
             </header>
             <main className='App-main'>
-                <Switch>
-                    <Route exact path='/' component={ MoviesList } />
-                    <Route path='/:id' component={ MovieDetails } />
-                </Switch>
+                <Suspense fallback={<div>Lazy router loading...</div>}>
+                    <Switch>
+                        <Route exact path='/' component={MoviesList} />
+                        <Route path='/:id' component={MovieDetails} />
+                    </Switch>
+                </Suspense>
             </main>
         </div>
     </Router>
